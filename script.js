ebb8ebdce77e324e822105a51005c2d733263558
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-05-31 16:28:58 +0530

    carousel mobile version complate

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 const picsDiv =  document.querySelector(".pics");
+let slideDirection = "right";
 
 picsDiv.addEventListener('click', (e) => {
     if (!(e.target.tagName === 'IMG')) {
         return;
     }
-    e.target.parentNode.classList.remove("main-pic-div");
-    const nextDiv = e.target.parentNode.nextElementSibling;
-    nextDiv.classList.remove("secondary-pic-div");
-    nextDiv.classList.add("main-pic-div");
 
-    console.log(nextDiv.nextElementSibling);
-    if (nextDiv.nextElementSibling === null) {
-        console.log(nextDiv.previousElementSibling);
-        nextDiv.classList.remove("translate");
-        nextDiv.classList.add("translate-twice");
+    const prevDiv = e.target.parentNode.previousElementSibling;
+
+    if (slideDirection === "left") {
+        if (e.target.id === "item-2") {
+            slideDirection = "right";
+        }
+
+        if (e.target.id === "item-3") {
+            e.target.parentNode.classList.add("translate");
+            e.target.parentNode.classList.remove("translate-twice");
+        } else {
+            e.target.parentNode.classList.remove("translate");
+        }
+        
+        prevDiv.classList.add("main-pic-div");
+        e.target.parentNode.classList.add("secondary-pic-div");
+        e.target.parentNode.classList.remove("main-pic-div");
         return;
     }
+    
+    const nextDiv = e.target.parentNode.nextElementSibling;
+    e.target.parentNode.classList.remove("main-pic-div");
+    nextDiv.classList.remove("secondary-pic-div");
+    nextDiv.classList.add("main-pic-div");
+    
+    if (nextDiv.nextElementSibling === null) {
+        nextDiv.classList.remove("translate");
+        nextDiv.classList.add("translate-twice");
+        slideDirection = "left";
+        return;
+    }
+
     nextDiv.classList.add("translate");
     nextDiv.nextElementSibling.classList.add("translate");
 });
-
-//! have to work on carousel
